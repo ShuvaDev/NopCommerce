ae23162c58520fa56edb6f631bd6c1965bf75d22
S.R. Shuva Dev <s.r.shuvadeb@gmail.com>
2026-06-03 18:49:57 +0600

    Add project files.

---
+++ Libraries/Nop.Core/Domain/Messages/MessagesSettings.cs
@@ -0,0 +1,26 @@
+﻿using Nop.Core.Configuration;
+
+namespace Nop.Core.Domain.Messages;
+
+/// <summary>
+/// Messages settings
+/// </summary>
+public partial class MessagesSettings : ISettings
+{
+    /// <summary>
+    /// A value indicating whether popup notifications set as default 
+    /// </summary>
+    public bool UsePopupNotifications { get; set; }
+
+    /// <summary>
+    /// A value indicating whether to use the default email account to send emails for store owner
+    ///
+    /// If set to false the message template email address will be use
+    /// </summary>
+    public bool UseDefaultEmailAccountForSendStoreOwnerEmails { get; set; }
+
+    /// <summary>
+    /// Gets or sets system name of active sms provider
+    /// </summary>
+    public string ActiveSmsProviderSystemName { get; set; }
+}
