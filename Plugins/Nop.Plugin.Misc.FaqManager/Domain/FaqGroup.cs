75a3be0c4dd40db42ad3ceb7cd063ab0fbf8e5b1
S.R. Shuva Dev <s.r.shuvadeb@gmail.com>
2026-06-12 11:04:06 +0600

    add FaqGroup and FaqItem entity

---
+++ Plugins/Nop.Plugin.Misc.FaqManager/Domain/FaqGroup.cs
@@ -0,0 +1,31 @@
+﻿using Nop.Core;
+using Nop.Core.Domain.Localization;
+using Nop.Core.Domain.Stores;
+
+namespace Nop.Plugin.Misc.FaqManager.Domain;
+
+/// <summary>
+/// Represents a FAQ group assigned to a product
+/// </summary>
+public class FaqGroup : BaseEntity, ILocalizedEntity
+{
+    /// <summary>
+    /// Group name
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Product identifier
+    /// </summary>
+    public int ProductId { get; set; }
+
+    /// <summary>
+    /// Whether the group is published
+    /// </summary>
+    public bool Published { get; set; }
+
+    /// <summary>
+    /// Display order
+    /// </summary>
+    public int DisplayOrder { get; set; }
+}
---
+++ Plugins/Nop.Plugin.Misc.FaqManager/Domain/FaqItem.cs
@@ -0,0 +1,35 @@
+﻿using Nop.Core;
+using Nop.Core.Domain.Localization;
+
+namespace Nop.Plugin.Misc.FaqManager.Domain;
+
+/// <summary>
+/// Represents a FAQ item (question & answer)
+/// </summary>
+public class FaqItem : BaseEntity, ILocalizedEntity
+{
+    /// <summary>
+    /// Parent FAQ group identifier
+    /// </summary>
+    public int FaqGroupId { get; set; }
+
+    /// <summary>
+    /// Question
+    /// </summary>
+    public string Question { get; set; }
+
+    /// <summary>
+    /// Answer (HTML content)
+    /// </summary>
+    public string Answer { get; set; }
+
+    /// <summary>
+    /// Whether the item is published
+    /// </summary>
+    public bool Published { get; set; }
+
+    /// <summary>
+    /// Display order
+    /// </summary>
+    public int DisplayOrder { get; set; }
+}
