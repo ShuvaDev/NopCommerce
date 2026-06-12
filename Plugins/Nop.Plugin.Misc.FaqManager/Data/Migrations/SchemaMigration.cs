59e315e13c82ca5d9d04b32009a4709b1a360b76
S.R. Shuva Dev <s.r.shuvadeb@gmail.com>
2026-06-12 18:25:48 +0600

    move migration

--- Plugins/Nop.Plugin.Misc.FaqManager/Data/Migrations/SchemaMigration.cs
+++ Plugins/Nop.Plugin.Misc.FaqManager/Data/Migrations/SchemaMigration.cs
@@ -3,7 +3,7 @@ using Nop.Data.Extensions;
 using Nop.Data.Migrations;
 using Nop.Plugin.Misc.FaqManager.Domain;
 
-namespace Nop.Plugin.Misc.FaqManager.Data.Mapping.Migrations;
+namespace Nop.Plugin.Misc.FaqManager.Data.Migrations;
 
 [NopMigration("2026-06-12 00:00:00", "Misc.FaqManager schema", MigrationProcessType.Installation)]
 public class SchemaMigration : Migration
