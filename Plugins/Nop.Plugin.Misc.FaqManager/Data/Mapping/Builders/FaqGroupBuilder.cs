d56d19de9b643ecd7497be65de7821a37eafe55f
S.R. Shuva Dev <s.r.shuvadeb@gmail.com>
2026-06-12 11:29:30 +0600

    add builder mapping for FaqGroup and FaqItem entity

---
+++ Plugins/Nop.Plugin.Misc.FaqManager/Data/Mapping/Builders/FaqGroupBuilder.cs
@@ -0,0 +1,30 @@
+﻿using FluentMigrator.Builders.Create.Table;
+using Nop.Core.Domain.Catalog;
+using Nop.Data.Extensions;
+using Nop.Data.Mapping.Builders;
+using Nop.Plugin.Misc.FaqManager.Domain;
+
+namespace Nop.Plugin.Misc.FaqManager.Data.Mapping.Builders;
+
+/// <summary>
+/// Represents a faq group entity builder
+/// </summary>
+public class FaqGroupBuilder : NopEntityBuilder<FaqGroup>
+{
+    #region Methods
+
+    /// <summary>
+    /// Apply entity configuration
+    /// </summary>
+    /// <param name="table">Create table expression builder</param>
+    public override void MapEntity(CreateTableExpressionBuilder table)
+    {
+        table
+            .WithColumn(nameof(FaqGroup.Name)).AsString(400).NotNullable()
+            .WithColumn(nameof(FaqGroup.ProductId)).AsInt32().ForeignKey<Product>()
+            .WithColumn(nameof(FaqGroup.DisplayOrder)).AsInt32().NotNullable()
+            .WithColumn(nameof(FaqGroup.Published)).AsBoolean().NotNullable();
+    }
+
+    #endregion
+}
---
+++ Plugins/Nop.Plugin.Misc.FaqManager/Data/Mapping/Builders/FaqItemBuilder.cs
@@ -0,0 +1,30 @@
+﻿using FluentMigrator.Builders.Create.Table;
+using Nop.Data.Extensions;
+using Nop.Data.Mapping.Builders;
+using Nop.Plugin.Misc.FaqManager.Domain;
+
+namespace Nop.Plugin.Misc.FaqManager.Data.Mapping.Builders;
+
+/// <summary>
+/// Represents a faq item entity builder
+/// </summary>
+public class FaqItemBuilder : NopEntityBuilder<FaqItem>
+{
+    #region Methods
+
+    /// <summary>
+    /// Apply entity configuration
+    /// </summary>
+    /// <param name="table">Create table expression builder</param>
+    public override void MapEntity(CreateTableExpressionBuilder table)
+    {
+        table
+            .WithColumn(nameof(FaqItem.Question)).AsString(int.MaxValue).NotNullable()
+            .WithColumn(nameof(FaqItem.Answer)).AsString(int.MaxValue).NotNullable()
+            .WithColumn(nameof(FaqItem.DisplayOrder)).AsInt32().NotNullable()
+            .WithColumn(nameof(FaqItem.Published)).AsBoolean().NotNullable()
+            .WithColumn(nameof(FaqItem.FaqGroupId)).AsInt32().ForeignKey<FaqGroup>();
+    }
+
+    #endregion
+}
